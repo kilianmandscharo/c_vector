41986fcbf349732928f2ec8f8330e41ad586ff48
Kilian <kilianmandscharo@gmail.com>
2023-10-30 16:00:53 +0100

    Add comments to the methods

--- vector.c
+++ vector.c
@@ -4,12 +4,22 @@
 
 #include "vector.h"
 
+/**
+ * Create a new vector with the specified initial capacity.
+ *
+ * @param capacity The initial capacity of the vector.
+ *                If capacity is less than 1, it will be set to 1.
+ * @return A pointer to the newly created vector.
+ *         Returns NULL if memory allocation fails.
+ */
 Vector *Vec_new(size_t capacity) {
   Vector *vec = (Vector *)malloc(sizeof(Vector));
 
   if (vec == NULL) {
     return NULL;
   }
+
+  capacity = (capacity < 1) ? 1 : capacity;
 
   int *data = (int *)malloc(capacity * sizeof(int));
 
@@ -24,6 +34,15 @@ Vector *Vec_new(size_t capacity) {
   return vec;
 }
 
+/**
+ * Deallocate the memory used by the vector and its data.
+ *
+ * This function releases the memory allocated for the vector structure and the
+ * data array. It is safe to call this function with a NULL pointer, in which
+ * case it has no effect.
+ *
+ * @param v A pointer to the vector to be deallocated. It can be NULL.
+ */
 void Vec_free(Vector *v) {
   if (v == NULL)
     return;
@@ -32,10 +51,37 @@ void Vec_free(Vector *v) {
   free(v);
 }
 
+/**
+ * Get the integer value at the specified index in the vector.
+ *
+ * If the provided index is out of bounds (i.e., less than 0 or greater than
+ * or equal to the vector's length), the behavior is undefined.
+ *
+ * @param v A pointer to the vector from which the integer is to be retrieved.
+ * @param index The index at which to retrieve the integer.
+ * @return The integer value at the specified index.
+ */
 int Vec_get(Vector *v, size_t index) { return v->data[index]; }
 
+/**
+ * Set the integer value at the specified index in the vector.
+ *
+ * If the provided index is out of bounds (i.e., less than 0 or greater than
+ * or equal to the vector's length), the behavior is undefined.
+ *
+ * @param v A pointer to the vector in which the integer value is to be set.
+ * @param index The index at which to set the integer.
+ * @param val The integer value to be set at the specified index.
+ */
 void Vec_set(Vector *v, size_t index, int val) { v->data[index] = val; }
 
+/**
+ * Print the elements of the vector to the standard output.
+ *
+ * If the provided vector is NULL, this function has no effect.
+ *
+ * @param v A pointer to the vector whose elements should be printed.
+ */
 void Vec_print(Vector *v) {
   if (v == NULL)
     return;
@@ -46,16 +92,29 @@ void Vec_print(Vector *v) {
   printf("\n");
 }
 
-void Vec_push(Vector *v, int val) {
+/**
+ * Push an integer onto the end of the vector.
+ *
+ * If the vector is full, it automatically reallocates memory to accommodate
+ * the new element, expanding its capacity by 5ß% if necessary. If reallocation
+ * fails, the function returns false, and the vector remains unchanged.
+ *
+ * @param v A pointer to the vector to which the integer should be pushed.
+ *          It can be NULL.
+ * @param val The integer value to be added to the end of the vector.
+ * @return true if the push operation is successful; false if reallocation fails
+ *         or if the provided vector pointer is NULL.
+ */
+bool Vec_push(Vector *v, int val) {
   if (v == NULL)
-    return;
+    return false;
 
   if (v->length >= v->capacity) {
     size_t new_capacity = v->capacity <= 1 ? 2 : (size_t)v->capacity * 1.5;
     int *new_data = (int *)realloc(v->data, new_capacity * sizeof(int));
 
     if (new_data == NULL) {
-      // TODO: add error handling
+      return false;
     }
 
     v->data = new_data;
@@ -63,8 +122,22 @@ void Vec_push(Vector *v, int val) {
   }
 
   v->data[v->length++] = val;
+
+  return true;
 }
 
+/**
+ * Remove and return the last element from the vector.
+ *
+ * If the provided vector is NULL or empty, the result's "success" field is set
+ * to false, and the "val" field is set to -1. Otherwise, the last element is
+ * removed from the vector, and the result's "success" field is set to true, and
+ * "val" holds the removed element's value.
+ *
+ * @param v A pointer to the vector from which to pop the last element.
+ * @return A PopResult structure indicating the success of the operation and the
+ *         removed element's value.
+ */
 PopResult Vec_pop(Vector *v) {
   PopResult result;
 
@@ -83,12 +156,37 @@ PopResult Vec_pop(Vector *v) {
   return result;
 }
 
+/**
+ * Swap the elements at the specified positions in the vector.
+ *
+ * This function swaps the elements at positions x and y within the vector. If
+ * either position is out of bounds (i.e., less than 0 or greater than or equal
+ * to the vector's length), the behavior is undefined.
+ *
+ * @param v A pointer to the vector containing the elements to be swapped.
+ * @param x The index of the first element to swap.
+ * @param y The index of the second element to swap.
+ */
 void Vec_swap(Vector *v, size_t x, size_t y) {
   int tmp = v->data[x];
   v->data[x] = v->data[y];
   v->data[y] = tmp;
 }
 
+/**
+ * Create a new vector that is a slice of the original vector.
+ *
+ * This function creates a new vector containing elements from the original
+ * vector within the specified range [left, right). If the provided vector is
+ * NULL or if the left index is greater than or equal to the right index, the
+ * function returns NULL.
+ *
+ * @param v A pointer to the original vector from which to create a slice.
+ * @param left The left index (inclusive) of the range to include in the slice.
+ * @param right The right index (exclusive) of the range to include in the
+ * slice.
+ * @return A pointer to the newly created slice vector or NULL on failure.
+ */
 Vector *Vec_slice(Vector *v, size_t left, size_t right) {
   if (left >= right || v == NULL)
     return NULL;
